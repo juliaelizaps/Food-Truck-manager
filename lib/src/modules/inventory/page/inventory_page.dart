83cbbf5971eab213aaedb05f33264485b1cf0ad7
Julia Eliza <juliaelizaps@gmail.com>
2024-10-24 20:34:55 -0300

    feat: finalizei a página de Estoque com as funcionalidades de Update e Delete do CRUD
    - Implementação das funcionalidades de Update e Delete na página de Estoque

--- lib/src/modules/inventory/page/inventory_page.dart
+++ lib/src/modules/inventory/page/inventory_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gf/src/modules/inventory/components/inventory_form_modal.dart';
-import 'package:gf/src/modules/inventory/model/inventory_model.dart'; // Certifique-se de que este caminho está correto
+import 'package:gf/src/modules/inventory/model/inventory_model.dart';
 import 'package:gf/src/shared/colors/colors.dart';
 import '../../../shared/components/sideBar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -37,37 +37,66 @@ class _InventoryPageState extends State<InventoryPage> {
           style: TextStyle(fontSize: 18),
         ),
       )
-          : ListView(
-        children: List.generate(
-          listInventory.length,
-              (index) {
+          : RefreshIndicator(
+        onRefresh: () {
+          return refresh();
+        },
+        child: ListView.builder(
+          itemCount: listInventory.length,
+          itemBuilder: (context, index) {
             Inventory model = listInventory[index];
-            return ListTile(
-              leading: const Icon(Icons.list_alt_rounded),
-              title: Text(model.name),
-              subtitle: Text('ID: ${model.id} - Quantidade: ${model.quantity}'), // Mostrando a quantidade também
+            return Dismissible(
+              key: ValueKey<Inventory>(model),
+              direction: DismissDirection.endToStart,
+              background: Container(
+                color: AppColors.sweepDeleteColor,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 8.0),
+                child: const Icon(
+                  Icons.delete_sweep,
+                  color: AppColors.deleteIconColor,
+                ) ,
+              ),
+              onDismissed: (direction){
+                remove(model);
+              },
+              child: ListTile(
+                onTap: () {
+                  //print('Clicou');
+                },
+                onLongPress: () => InventoryFormModal.showFormModal(context, bdFirebase, refresh, model: model),
+                leading: const Icon(Icons.inventory_outlined),
+                title: Text(model.name),
+                subtitle: Text('Quantidade: ${model.quantity} \nID: ${model.id} '),
+              ),
             );
           },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => InventoryFormModal.showFormModal(context, bdFirebase, refresh), // Usando a classe InventoryFormModal
+        onPressed: () => InventoryFormModal.showFormModal(context, bdFirebase, refresh),
         backgroundColor: AppColors.buttonColor,
         child: const Icon(Icons.add),
       ),
     );
   }
-  refresh() async{
-    List<Inventory> temp =[];
 
+  refresh() async {
+    List<Inventory> temp = [];
     QuerySnapshot<Map<String, dynamic>> snapshot =
     await bdFirebase.collection('Estoque').get();
-    for (var doc in snapshot.docs){
-     temp.add(Inventory.fromMap(doc.data()));
+    for (var doc in snapshot.docs) {
+      temp.add(Inventory.fromMap(doc.data()));
     }
-    setState((){
+    setState(() {
       listInventory = temp;
     });
   }
+
+  void remove(Inventory model){
+    bdFirebase.collection('Estoque').doc(model.id).delete();
+    refresh();
+  }
+
 }
 
