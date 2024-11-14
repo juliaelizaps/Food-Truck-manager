b14de88aca6277f49e7ff1fccaed246257e969a8
Julia Eliza <juliaelizaps@gmail.com>
2024-11-14 19:14:02 -0300

    feat: adicionado funções para auxiliar na autenticação com firebase, criado campos para criar usuário

--- lib/src/modules/order/components/order_form_modal.dart
+++ lib/src/modules/order/components/order_form_modal.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
 import 'package:gf/src/modules/order/model/order_model.dart' as order_model;
+
+import '../../../shared/components/red_button.dart';
 
 class OrderFormModal extends StatefulWidget {
   final Function(order_model.OrderProduct, String, List<order_model.OrderProduct>) onAddToCart;
@@ -13,10 +16,10 @@ class OrderFormModal extends StatefulWidget {
 
 class _OrderFormModalState extends State<OrderFormModal> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  String comentarioProduto = "";
-  List<order_model.OrderProduct> adicionaisProduto = [];
-  order_model.OrderProduct? produtoSelecionado;
-  List<order_model.OrderProduct> produtos = [];
+  String productObservation = "";
+  List<order_model.OrderProduct> additionalProduct = [];
+  order_model.OrderProduct? selectedProduct;
+  List<order_model.OrderProduct> products = [];
 
   @override
   void initState() {
@@ -27,7 +30,7 @@ class _OrderFormModalState extends State<OrderFormModal> {
   Future<void> fetchProdutos() async {
     QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore.collection('Produtos').get();
     setState(() {
-      produtos = snapshot.docs.map((doc) => order_model.OrderProduct.fromMap(doc.data()!)).toList();
+      products = snapshot.docs.map((doc) => order_model.OrderProduct.fromMap(doc.data()!)).toList();
     });
   }
 
@@ -41,7 +44,7 @@ class _OrderFormModalState extends State<OrderFormModal> {
           Text("Adicionar Produto", style: Theme.of(context).textTheme.headline5),
           DropdownButtonFormField<order_model.OrderProduct>(
             decoration: const InputDecoration(labelText: "Selecionar Produto"),
-            items: produtos.map((produto) {
+            items: products.map((produto) {
               return DropdownMenuItem<order_model.OrderProduct>(
                 value: produto,
                 child: Text(produto.name),
@@ -49,7 +52,7 @@ class _OrderFormModalState extends State<OrderFormModal> {
             }).toList(),
             onChanged: (newValue) {
               setState(() {
-                produtoSelecionado = newValue;
+                selectedProduct = newValue;
               });
             },
           ),
@@ -58,63 +61,66 @@ class _OrderFormModalState extends State<OrderFormModal> {
             decoration: const InputDecoration(labelText: "Observação"),
             onChanged: (value) {
               setState(() {
-                comentarioProduto = value;
+                productObservation = value;
               });
             },
           ),
           const SizedBox(height: 16),
           DropdownButtonFormField<String>(
             decoration: const InputDecoration(labelText: "Selecionar Adicional"),
-            items: produtos.map((produto) {
+            items: products.map((produto) {
               return DropdownMenuItem<String>(
                 value: produto.id,
                 child: Text(produto.name),
               );
             }).toList(),
             onChanged: (String? newValue) {
-              if (newValue != null && !adicionaisProduto.any((produto) => produto.id == newValue)) {
+              if (newValue != null && !additionalProduct.any((produto) => produto.id == newValue)) {
                 setState(() {
-                  var matchedProduct = produtos.firstWhere((produto) => produto.id == newValue);
-                  adicionaisProduto.add(matchedProduct);
+                  var matchedProduct = products.firstWhere((produto) => produto.id == newValue);
+                  additionalProduct.add(matchedProduct);
                 });
               }
             },
           ),
-          const SizedBox(height: 16),
-          Wrap(
-            children: adicionaisProduto.map((produto) {
-              return Chip(
-                label: Text(produto.name),
-                onDeleted: () {
-                  setState(() {
-                    adicionaisProduto.remove(produto);
-                  });
-                },
-              );
-            }).toList(),
+          const SizedBox(height: 10),
+          Expanded(
+            //constraints: const BoxConstraints(maxHeight: 200),
+            child: ListView(
+              //shrinkWrap: true,
+              children: additionalProduct.map((produto) {
+                return Chip(
+                  label: Text(produto.name),
+                  onDeleted: () {
+                    setState(() {
+                      additionalProduct.remove(produto);
+                    });
+                  },
+                );
+              }).toList(),
+            ),
           ),
-          const Spacer(),
+          //const Spacer(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               ElevatedButton(
                 onPressed: () {
-                  if (produtoSelecionado != null) {
+                  Navigator.pop(context);
+                }, child: const Text("Cancelar"),
+              ),
+              RedButton(
+                onPressed: () {
+                  if (selectedProduct != null) {
                     widget.onAddToCart(
-                      produtoSelecionado!,
-                      comentarioProduto,
-                      adicionaisProduto,
+                      selectedProduct!,
+                      productObservation,
+                      additionalProduct,
                     );
                     Navigator.pop(context);
                   }
                 },
-                child: const Text("Adicionar ao Carrinho"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Cancelar"),
+                text: 'Adicionar',
               ),
             ],
           ),
